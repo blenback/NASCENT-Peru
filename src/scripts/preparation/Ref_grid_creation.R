919a6303efbe3e0b538842414592c6982d58d4eb
Chenyu <chshen@ethz.ch>
2023-12-29 12:09:12 +0100

    update ref gird creation script

--- src/scripts/preparation/Ref_grid_creation.R
+++ src/scripts/preparation/Ref_grid_creation.R
@@ -59,27 +59,27 @@ writeRaster(cropped_gird, "ref_grid.tif", overwrite = TRUE)
 ### C - Calculate the number of pixels within the Peru boundary 
 ### =========================================================================
 
-# Define the number of chunks (adjust this value as needed, more chunks for larger rasters)
-num_chunks <- 100
-
-# Get the number of rows per chunk
-rows_per_chunk <- ceiling(nrow(input_raster) / num_chunks)
-
-# Initialize the count
-non_zero_pixels <- 0
-
-# Loop over each chunk
-for (i in 1:num_chunks) {
-  # Define the start and end rows for the current chunk
-  start_row <- ((i-1) * rows_per_chunk) + 1
-  end_row <- min(i * rows_per_chunk, nrow(input_raster))
-  
-  # Retrieve the values for the chunk
-  chunk <- input_raster[start_row:end_row, ]
-  
-    
-  # Sum the non-zero values in the chunk and add to the total count
-  non_zero_pixels <- non_zero_pixels + sum(chunk != 0, na.rm = TRUE)
-}
-
-print(non_zero_pixels)
+# # Define the number of chunks (adjust this value as needed, more chunks for larger rasters)
+# num_chunks <- 100
+# 
+# # Get the number of rows per chunk
+# rows_per_chunk <- ceiling(nrow(input_raster) / num_chunks)
+# 
+# # Initialize the count
+# non_zero_pixels <- 0
+# 
+# # Loop over each chunk
+# for (i in 1:num_chunks) {
+#   # Define the start and end rows for the current chunk
+#   start_row <- ((i-1) * rows_per_chunk) + 1
+#   end_row <- min(i * rows_per_chunk, nrow(input_raster))
+#   
+#   # Retrieve the values for the chunk
+#   chunk <- input_raster[start_row:end_row, ]
+#   
+#     
+#   # Sum the non-zero values in the chunk and add to the total count
+#   non_zero_pixels <- non_zero_pixels + sum(chunk != 0, na.rm = TRUE)
+# }
+# 
+# print(non_zero_pixels)
