120d90f3934550801dca62ab0dd3254f1e2c11b3
blenback <bblack@ethz.ch>
2024-02-16 16:26:46 +0100

    results of focal testing

--- src/scripts/LULCC/dev/test_focal_on_30m_rast.R
+++ src/scripts/LULCC/dev/test_focal_on_30m_rast.R
@@ -25,4 +25,22 @@ invisible(lapply(packs, require, character.only = TRUE))
 # Source custom functions
 invisible(sapply(list.files("Scripts/Functions",pattern = ".R",full.names = TRUE,recursive = TRUE),source))
 
+# path to test layer
+test_lyr_path <- "X:/NASCENT-Peru/03_workspaces/02_modelling/Data_collab/LULC/MapBiomas/peru_coverage_1985.tif"
+
+# load test layer
+test_lyr <- rast(test_lyr_path)
+
+#start timer
+start_time <- Sys.time()
+
+#run focal calc
+test_lyr_focal <- focal(test_lyr, w=matrix(1,3,3), fun=mean, na.rm=TRUE)
+
+#end timer
+end_time <- Sys.time()
+
+#calculate time
+time_taken <- end_time - start_time
+
 
